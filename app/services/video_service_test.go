1a71b2705085deb1cbfbfbfc1e164043a43a6f73
wellmtx <wellingtonsaraivasousa@gmail.com>
2023-05-20 23:54:56 -0300

    feat: :sparkles: Download And Fragment

---
+++ app/services/video_service_test.go
@@ -0,0 +1,52 @@
+package services_test
+
+import (
+	"log"
+	"testing"
+	"time"
+	"videoEncoder/app/repositories"
+	"videoEncoder/app/services"
+	"videoEncoder/domain"
+	"videoEncoder/framework/database"
+
+	"github.com/joho/godotenv"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+func prepare() (*domain.Video, repositories.VideoRepository) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "dayz.mp4"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+	repo.Insert(video)
+
+	return video, repo
+}
+
+func TestVideoService_Download(t *testing.T) {
+	video, repo := prepare()
+
+	videoService := services.NewVideoService()
+	videoService.Video = video
+	videoService.VideoRepository = repo
+
+	err := videoService.Download("codeeducationencoder")
+	require.Nil(t, err)
+
+	err = videoService.Fragment()
+	require.Nil(t, err)
+}
